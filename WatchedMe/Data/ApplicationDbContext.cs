542d281be3d51afeff6800a3060a70a0b178df09
Adrian Degenaar <adriandegenaar69@gmail.com>
2023-01-14 12:28:15 +0200

    Add project files.

---
+++ WatchedMe/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WatchedMe.Models;
+
+namespace WatchedMe.Data {
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser> {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions) {
+
+        }
+    }
+}
---
+++ WatchedMe/Models/ApplicationUser.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WatchedMe.Models {
+    public class ApplicationUser : IdentityUser {
+    }
+}
