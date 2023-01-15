b06277d1704f3883a7a143391cfd8fd783ecc9fb
Adrian Degenaar <adriandegenaar69@gmail.com>
2023-01-16 01:58:30 +0200

    Cleaning up unused models

--- WatchedMe/Models/NoDb/ErrorReply.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WatchedMe.Models.NoDb {
-    public class ErrorReply {
-        public bool IsError { get; set; }
-        public string Message { get; set; } = string.Empty;
-    }
-}
--- WatchedMe/Models/NoDb/MovieReturnMessage.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WatchedMe.Models.NoDb {
-    public class MovieReturnMessage {
-        public ErrorReply ErrorInfo { get; set; } = new ErrorReply();
-        public List<Movie> MovieList { get; set; } = new List<Movie>();
-        public Movie SingleMovie { get; set; } = new Movie();
-    }
-}
